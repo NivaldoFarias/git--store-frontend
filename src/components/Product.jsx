670246f59359fda84f71d18a0784b9e190f75b28
Nivaldo Farias <badivia.f@gmail.com>
2022-05-13 11:53:16 -0300

    Feat: Update cart useState to use CartContext

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,44 +1,52 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
-import {useContext} from 'react';
+import axios from "axios";
+import dotenv from "dotenv";
+import { useContext } from "react";
 
-import TokenContext from './../hooks/TokenContext';
+import CartContext from "./../hooks/CartContext";
+import TokenContext from "./../hooks/TokenContext";
 
 dotenv.config();
 
-export default function Product({product, cart, setCart}) {
-    const {token} = useContext(TokenContext);
+export default function Product({ product }) {
+  const { image, title, price, _id } = product;
+  const { cart, setCart } = useContext(CartContext);
+  const { token } = useContext(TokenContext);
 
-    function addToCart() {
-        const URL = process.env.REACT_APP_API_URL;
-        const item = {...product, quantity: 1};
-        axios.put(`${URL}/users/cart`, item, {
-            headers: {
-                authorization: `Bearer ${token}`
-            }
-        } ).then(response => {
-            const productIndex = cart.findIndex(item => item._id === product._id);
-            
-            if(productIndex === -1) {
-                setCart([...cart, response.data]);
-                return;
-            }
+  function addToCart() {
+    const URL = process.env.API_URL;
+    const item = { ...product, quantity: 1 };
+    axios
+      .put(`${URL}/users/cart`, item, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((_res) => {
+        const productIndex = cart.findIndex((item) => item._id === _id);
 
-            cart[productIndex].quantity += 1;
-            setCart([...cart]);
-            
-        })
-    }
+        if (productIndex === -1) {
+          setCart([...cart, item]);
+          return;
+        }
 
-    return (
-        <article className='product'>
-            <a href="#"><img src={product.image} alt={product.title} /></a>
-            <h1>{product.title}</h1>
-            <span>{product.price}</span>
-            <button onClick={addToCart}>Adicionar ao carrinho</button>
-        </article>
-        )
+        cart[productIndex].quantity += 1;
+        setCart([...cart]);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }
 
+  return (
+    <article className="product">
+      <a href="#">
+        <img src={image} alt={title} />
+      </a>
+      <h1>{title}</h1>
+      <span>{price}</span>
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
+    </article>
+  );
 }
 
 /*
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //import { DataProvider } from "./../hooks/DataContext";
 import { useLocalStorage } from "./../hooks/useLocalStorage";
+import CartContext from "./../hooks/CartContext";
 import TokenContext from "./../hooks/TokenContext";
 
 import Signin from "./Signin";
@@ -10,17 +11,20 @@ import SignUp from "./Signup";
 import Home from "./Home";
 
 export default function App() {
+  const [cart, setCart] = useState([]);
   const [token, setToken] = useLocalStorage("token", null);
 
   return (
-    <TokenContext.Provider value={{ token, setToken }}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Signin />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </TokenContext.Provider>
+    <CartContext.Provider value={{ cart, setCart }}>
+      <TokenContext.Provider value={{ token, setToken }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Signin />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </TokenContext.Provider>
+    </CartContext.Provider>
   );
 }
