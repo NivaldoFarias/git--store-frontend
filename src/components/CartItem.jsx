41b5dab00d733ff911fa2fa822fb836cc9c0395e
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-05-16 04:22:07 -0300

    Create CartItem component

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState, useContext } from 'react';
+
+import CartContext from '../hooks/CartContext';
+
+export default function CartItem({ product }) {
+  console.log(product);
+  const { price, title, product_id, image_url } = product;
+  let { volume } = product;
+  const { cart, setCart } = useContext(CartContext);
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {
+    setQuantity(volume);
+    if (volume === 0) {
+      const newCart = cart.filter((item) => item.product_id !== product_id);
+      setCart(newCart);
+    }
+  }, [volume]);
+
+  function addOne() {
+    setQuantity(quantity + 1);
+    volume = quantity;
+    const newCart = cart.map((item) => {
+      if (item.product_id === product_id) item.volume++;
+      return item;
+    });
+    setCart(newCart);
+  }
+
+  function subOne() {
+    setQuantity(quantity - 1);
+    volume = quantity;
+    const newCart = cart.map((item) => {
+      if (item.product_id === product_id) item.volume--;
+      return item;
+    });
+    setCart(newCart);
+  }
+
+  return (
+    <div className="cart-modal__item">
+      <a href="#">
+        <img src={image_url} alt={title} />
+      </a>
+      <h1>{title}</h1>
+      <div className="cart-modal__item__info">
+        <span>R$ {price} × </span>
+        <button onClick={subOne}>-</button>
+        <input
+          type="number"
+          step="1"
+          min="0"
+          value={quantity}
+          onChange={(e) => {
+            setQuantity(e.target.value);
+            volume = quantity;
+            const newCart = cart.map((item) => {
+              if (item.product_id === product_id) item.volume = e.target.value;
+              return item;
+            });
+            setCart(newCart);
+          }}
+        />
+        <button onClick={addOne}>+</button>
+      </div>
+      <span>subtotal - R$ {(price * quantity).toFixed(2)}</span>
+    </div>
+  );
+}
